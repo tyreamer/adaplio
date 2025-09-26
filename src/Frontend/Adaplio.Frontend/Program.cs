b3798fc1efe96d47f484927d538447350d3d4d0f
tyreamer <tyreamer@gmail.com>
2025-09-26 09:40:27 -0400

    fix cookies

--- src/Frontend/Adaplio.Frontend/Program.cs
+++ src/Frontend/Adaplio.Frontend/Program.cs
@@ -8,8 +8,20 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient {
-    BaseAddress = new Uri("https://adaplio.onrender.com") // Production API URL
+builder.Services.AddScoped(sp =>
+{
+    var httpClient = new HttpClient(new HttpClientHandler()
+    {
+        UseCookies = true
+    })
+    {
+        BaseAddress = new Uri("https://adaplio.onrender.com") // Production API URL
+    };
+
+    // Ensure credentials (cookies) are included in cross-origin requests
+    httpClient.DefaultRequestHeaders.Add("credentials", "include");
+
+    return httpClient;
 });
 builder.Services.AddMudServices();
 builder.Services.AddThemeService();
